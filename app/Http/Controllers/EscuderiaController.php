aebe7e148225c8242ba377773504204e0637ff44
Lucia <larnedo@hiberus.com>
2023-03-23 13:39:25 +0100

    Escuderia controller working

---
+++ app/Http/Controllers/EscuderiaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Escuderia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class EscuderiaController extends Controller
+{
+    public function index() {
+        return Escuderia::all();
+    }
+
+    public function show($id) {
+        $escuderia = Escuderia::findOrFail($id);
+
+        return response()->json(['escuderia' => $escuderia], 200);
+    }
+    public function store(Request $request) {
+        $validate = Validator::make($request->all(), [
+            'nombre' => 'required|string',
+            'director' => 'required|string',
+            'motorUsado' => 'required|string',
+            'image' => 'required'
+        ]);
+
+        if($validate->fails()) {
+            return response()->json(['error' => 'Hubo un error al almacenar la escuderia.'], 500);
+        }
+
+        $input = $request->all();
+
+        if($file = $request->file('image')) {
+            $name = $file->getClientOriginalName();
+            Storage::put('public/escuderias/' . $name, file_get_contents($file->getRealPath()));
+            $input['image'] = $name;
+        }
+
+        $escuderia = new Escuderia($input);
+        $escuderia->image = $input['image'];
+        $escuderia->save();
+        return response()->json(['success' => 'Escuderia guardada con exito', 'escuderia' => $escuderia], 200);
+    }
+
+    public function update(Request $request, $id) {
+        $escuderia = Escuderia::findOrFail($id);
+
+        $input = $request->all();
+        $escuderia->update($input);
+
+        if($file = $request->file('image')) {
+            $name = $file->getClientOriginalName();
+            Storage::put('public/escuderias/' . $name, file_get_contents($file->getRealPath()));
+            $escuderia->image = $name;
+            $escuderia->save();
+        }
+
+        return response()->json(['success' => 'Escuderia editada con exito', 'escuderia' => $escuderia], 200);
+    }
+
+    public function delete($id) {
+        $escuderia = Escuderia::findOrFail($id);
+        $escuderia->delete();
+        return response()->json(['success' => 'Escudería borrada con exito', 'escudería' => $escuderia], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EscuderiaController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,9 @@ Route::get('rol/{id}', [RoleController::class, 'show']);
 Route::get('roles', [RoleController::class, 'index']);
 Route::put('rol/{id}', [RoleController::class, 'update']);
 Route::delete('rol/{id}', [RoleController::class, 'delete']);
+
+Route::post('escuderia', [EscuderiaController::class, 'store']);
+Route::get('escuderia/{id}', [EscuderiaController::class, 'show']);
+Route::get('escuderias', [EscuderiaController::class, 'index']);
+Route::put('escuderia/{id}', [EscuderiaController::class, 'update']);
+Route::delete('escuderia/{id}', [EscuderiaController::class, 'delete']);
