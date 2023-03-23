ce8190453350872c1add25a45e8b052bd8880fc9
Lucia <larnedo@hiberus.com>
2023-03-23 13:03:24 +0100

    Role controller working

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RoleController extends Controller
+{
+    public function index() {
+        return Role::all();
+    }
+
+    public function show($id) {
+        $rol = Role::findOrFail($id);
+
+        return response()->json(['rol' => $rol], 200);
+    }
+    public function store(Request $request) {
+        $validate = Validator::make($request->all(), [
+            'nombre' => 'required|string'
+        ]);
+
+        if($validate->fails()) {
+            return response()->json(['error' => 'Hubo un error al almacenar el rol.'], 500);
+        }
+
+        $input = $request->all();
+
+        $rol = new Role($input);
+        $rol->save();
+        return response()->json(['success' => 'Rol guardado con exito', 'rol' => $rol], 200);
+    }
+
+    public function update(Request $request, $id) {
+        $rol = Role::findOrFail($id);
+
+        $rol->update($request->all());
+        return response()->json(['success' => 'Rol editado con exito', 'rol' => $rol], 200);
+    }
+
+    public function delete($id) {
+        $rol = Role::findOrFail($id);
+
+        $rol->delete();
+        return response()->json(['success' => 'Rol borrado con exito', 'rol' => $rol], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('rol', [RoleController::class, 'store']);
+Route::get('rol/{id}', [RoleController::class, 'show']);
+Route::get('roles', [RoleController::class, 'index']);
+Route::put('rol/{id}', [RoleController::class, 'update']);
+Route::delete('rol/{id}', [RoleController::class, 'delete']);
